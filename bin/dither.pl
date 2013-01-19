9b29ec6806527364e03e9b88bb601a94cb7b3d68
straup <straup@gmail.com>
2013-01-19 13:15:52 -0500

    start looping through the image

--- bin/dither.pl
+++ bin/dither.pl
@@ -10,7 +10,39 @@ use Imager;
 
 sub main {
 
-    my $infile = $ARGV[1];
-    my $im = Imager->new(file => $infile);
+    my $infile = $ARGV[0];
+    my $outfile = $ARGV[1];
+
+    dither($infile, $outfile);
+    return 1;
+}
+
+sub dither {
+    my $infile = shift;
+    my $outfile = shift;
+
+    my @threshold = ();
+    push @threshold, (0) x 128;
+    push @threshold, (255) x 128;
+
+    my $im = Imager->new();
+    $im->read(file => $infile);
+
+    $im = $im->convert(preset => 'gray');
+
+    my $height = $im->getheight();
+    my $width = $im->getwidth();
+
+    for (my $y=0; $y < $height; $y++){
+
+	for (my $x=0; $x < $width; $x++){
+
+	    my $old = $im->getpixel(x => $x, y => $y);
+	    print $old->rgba() . "\n";
+	}
+    }
+
+    # $im = $im->convert(preset => 'rgba');
+    # $im->save($outfile);
 
 }
